455446bd601ab4c69daaa5f0c9f0f840deb00aef
Lakanna <134102704+Lakanna@users.noreply.github.com>
2024-07-29 23:28:19 +0000

    Deploying to gh-pages from @ Lakanna/js-hw-10@3f3fbea114a65b9f501917ef918c3da05b2c79eb 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/styles-ebc16479.js";import"./assets/vendor-77e16229.js";const t={form:document.querySelector(".js-form"),btnSubmit:document.querySelector("button")};console.dir(t.form);t.form.addEventListener("click",r);t.form.addEventListener("submit",o);function o(e){e.preventDefault,console.log(e.target)}function r(e){console.log("click",e.target)}
+//# sourceMappingURL=commonHelpers2.js.map
